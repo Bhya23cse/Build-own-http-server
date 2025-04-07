d3f4a1eb5abd172eac2710b89700bd89d654a618
Bhola Yadav <bhya23cse@gmail.com>
2025-04-07 23:02:24 +0530

    Updated Code to Extract URL Path

--- app/main.js
+++ app/main.js
@@ -3,8 +3,17 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
-        const [requestLine] = request.split("\r\n");
+        const [requestLine, ...headersAndBody] = request.split("\r\n");
+
         const [method, path] = requestLine.split(" ");
+
+        // Extract headers
+        const headers = {};
+        for (const line of headersAndBody) {
+            if (line === "") break; // end of headers
+            const [key, value] = line.split(": ");
+            headers[key] = value;
+        }
 
         let response = "";
 
@@ -20,6 +29,16 @@ const server = net.createServer((socket) => {
                 `Content-Length: ${contentLength}\r\n` +
                 "\r\n" +
                 echoText;
+        } else if (method === "GET" && path === "/user-agent") {
+            const userAgent = headers["User-Agent"] || "";
+            const contentLength = Buffer.byteLength(userAgent);
+
+            response =
+                "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                `Content-Length: ${contentLength}\r\n` +
+                "\r\n" +
+                userAgent;
         } else {
             response = "HTTP/1.1 404 Not Found\r\n\r\n";
         }
