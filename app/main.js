cf50a2578fc53283d2e43d39b46bc1dc1940fc3e
Bhola Yadav <bhya23cse@gmail.com>
2025-04-07 22:00:06 +0530

    reposnd 200

--- app/main.js
+++ app/main.js
@@ -1,13 +1,30 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.on("data", (data) => {
+        // Log the incoming request (optional)
+        console.log("Received request:\n" + data.toString());
+
+        // Basic HTTP response with status 200 OK
+        const response = 
+            "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/plain\r\n" +
+            "Content-Length: 13\r\n" +
+            "\r\n" +
+            "Hello, World!";
+
+        // Send the response
+        socket.write(response);
+        socket.end();
+    });
+
+    socket.on("close", () => {
+        console.log("Connection closed");
+    });
 });
-//
-server.listen(4221, "localhost");
+
+server.listen(4221, "localhost", () => {
+    console.log("Server is listening on port 4221");
+});
