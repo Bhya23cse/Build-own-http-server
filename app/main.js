232d8d8327ab132079c340e4d3dc1e89a9577f7a
Bhola Yadav <bhya23cse@gmail.com>
2025-04-07 22:32:47 +0530

    Updated Code to Extract URL Path

--- app/main.js
+++ app/main.js
@@ -3,35 +3,24 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
-        console.log("Raw Request:\n" + request);
-
-        // Extract the first line of the request (e.g., "GET /some/path HTTP/1.1")
         const [requestLine] = request.split("\r\n");
-        
-        // Split the request line into method, path, and HTTP version
-        const [method, path, httpVersion] = requestLine.split(" ");
+        const [method, path] = requestLine.split(" ");
 
-        console.log(`Method: ${method}`);
-        console.log(`Path: ${path}`);
-        console.log(`HTTP Version: ${httpVersion}`);
+        console.log(`Incoming Request: ${method} ${path}`);
 
-        // Response
+        const body = "Hello from custom HTTP server!";
         const response = 
             "HTTP/1.1 200 OK\r\n" +
             "Content-Type: text/plain\r\n" +
-            "Content-Length: 13\r\n" +
+            `Content-Length: ${body.length}\r\n` +
             "\r\n" +
-            "Hello, World!";
+            body;
 
         socket.write(response);
         socket.end();
     });
-
-    socket.on("close", () => {
-        console.log("Connection has closed");
-    });
 });
 
 server.listen(4221, "localhost", () => {
-    console.log("Server is listening on port 4221");
+    console.log("Server running at http://localhost:4221");
 });
