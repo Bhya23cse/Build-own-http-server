275a45341f06a43028bba291a4b7bc12cd278510
Bhola Yadav <bhya23cse@gmail.com>
2025-04-07 22:38:51 +0530

    Updated Code to Extract URL Path

--- app/main.js
+++ app/main.js
@@ -3,13 +3,16 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
+
+        // Parse request line
         const [requestLine] = request.split("\r\n");
         const [method, path] = requestLine.split(" ");
 
         console.log(`Incoming Request: ${method} ${path}`);
 
+        // Create a basic response
         const body = "Hello from custom HTTP server!";
-        const response = 
+        const response =
             "HTTP/1.1 200 OK\r\n" +
             "Content-Type: text/plain\r\n" +
             `Content-Length: ${body.length}\r\n` +
@@ -22,5 +25,5 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost", () => {
-    console.log("Server running at http://localhost:4221");
+    console.log("Server is running at http://localhost:4221");
 });
